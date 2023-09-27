2ad26b0d4d4af731e8086a8f816a53f892e3f02f
Horacio Mota <horaciomota93@gmail.com>
2023-09-27 20:00:47 +0100

    First commit

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Horacio Mota on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
