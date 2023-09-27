68ed2799567c0090566cdd72fe23cdc73f916f98
Horacio Mota <horaciomota93@gmail.com>
2023-09-27 20:08:52 +0100

    Build path/folder added

--- CryptoWallet/Views/ContentView.swift
+++ CryptoWallet/Views/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        //Login Image - Rounded
+        
+        //Account balance
+        
+        //List of coins
+        
+        //List of expenses
+        
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
-        .padding()
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
